83655953013187d5f51874942e18fa5046c8004d
さいかい <70578583+saikai-moviestudio@users.noreply.github.com>
2021-07-30 16:24:06 +0900

    ok

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,13 +7,14 @@ import marked from "marked";
 import { loadGoogleScript } from "./GoogleLogin";
 const googleClientId =
   "910124176690-go4g2s66msipo9bfdmr8gr4232v9e78i.apps.googleusercontent.com";
-const readMemoUrl =
+const memoApiUrl =
   "https://script.googleapis.com/v1/scripts/AKfycbwCXY7YKm7S1VJv5xGItcsOJuT0JLuvM7wyrm8rrY6H9lcPZ99hGiU3MrRlwV6CKqXV7Q:run";
 const API_KEY = "AIzaSyDeOz1nvw1M6om8N9xcoVJaXq2B8oDfgO0";
 const scope = "https://www.googleapis.com/auth/spreadsheets";
 
 const Home = () => {
-  const [memo, setMemo] = useState("# メモ");
+  const [memo, setMemo] = useState("# メモ \n```\n初めまして、私は\n```");
+
   const [gapi, setGapi] = useState();
   const [googleAuth, setGoogleAuth] = useState();
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -74,13 +75,31 @@ const Home = () => {
   });
 
   const saveMemo = (m) => {
-    setMemo(m);
+    axios({
+      method: "post",
+      url: memoApiUrl,
+      headers: {
+        Authorization: "Bearer " + googleAuth,
+        "Content-Type": "application/json",
+      },
+      data: {
+        function: "addMemo",
+        parameters: [memo],
+      },
+    })
+      .then((res) => {
+        console.log("上書きテキスト：" + res);
+        setMemo(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
 
   const readMemo = () => {
     axios({
       method: "post",
-      url: readMemoUrl,
+      url: memoApiUrl,
       headers: {
         Authorization: "Bearer " + googleAuth,
         "Content-Type": "application/json",
@@ -104,13 +123,20 @@ const Home = () => {
       {!isLoggedIn && <div id="google-signin"></div>}
       <div className="homeMemo">
         <div className="homeMemoBoxEdit">
-          <SimpleMDE value={memo} onChange={(e) => saveMemo(e)} />
+          <SimpleMDE value={memo} onChange={(e) => setMemo(e)} />
           <button
             onClick={() => {
               readMemo();
             }}
           >
             同期
+          </button>
+          <button
+            onClick={() => {
+              saveMemo();
+            }}
+          >
+            上書き
           </button>
         </div>
         <div className="homeMemoBoxView">
