96c70781419d593b5f2aa3785c9dacfbc7b5f7da
さいかい <70578583+saikai-moviestudio@users.noreply.github.com>
2021-07-30 14:57:33 +0900

    test

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,44 +10,24 @@ const googleClientId =
 
 const Home = () => {
   const [memo, setMemo] = useState("");
-  const [isSignedIn, setIsSignedIn] = useState(false);
-  const CLIENT_ID =
-    "910124176690-go4g2s66msipo9bfdmr8gr4232v9e78i.apps.googleusercontent.com";
-  const API_KEY = "AIzaSyDeOz1nvw1M6om8N9xcoVJaXq2B8oDfgO0";
-  const scriptId = "1mVWj2_ScthdTtdrunyoGSZcfop9JyddBMmpLJUUU1P2F0A4MK7EgL5NF";
-  const SCOPES = ["https://www.googleapis.com/auth/spreadsheets"];
-
   const [gapi, setGapi] = useState();
   const [googleAuth, setGoogleAuth] = useState();
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [imageUrl, setImageUrl] = useState();
+  const [name, setName] = useState("name");
+  const [email, setEmail] = useState("email");
 
   const onSuccess = (googleUser) => {
-    // (Ref. 7)
     setIsLoggedIn(true);
     const profile = googleUser.getBasicProfile();
     setName(profile.getName());
     setEmail(profile.getEmail());
-    setImageUrl(profile.getImageUrl());
   };
 
   const onFailure = () => {
     setIsLoggedIn(false);
   };
 
-  const logOut = () => {
-    // (Ref. 8)
-    (async () => {
-      await googleAuth.signOut();
-      setIsLoggedIn(false);
-      renderSigninButton(gapi);
-    })();
-  };
-
   const renderSigninButton = (_gapi) => {
-    // (Ref. 6)
     _gapi.signin2.render("google-signin", {
       scope: "profile email",
       width: 240,
@@ -60,28 +40,41 @@ const Home = () => {
   };
 
   useEffect(() => {
-    // Window.gapi is available at this point
     window.onGoogleScriptLoad = () => {
-      // (Ref. 1)
-
-      const _gapi = window.gapi; // (Ref. 2)
+      const _gapi = window.gapi;
       setGapi(_gapi);
 
       _gapi.load("auth2", () => {
-        // (Ref. 3)
         (async () => {
           const _googleAuth = await _gapi.auth2.init({
-            // (Ref. 4)
             client_id: googleClientId,
           });
-          setGoogleAuth(_googleAuth); // (Ref. 5)
-          renderSigninButton(_gapi); // (Ref. 6)
+          setGoogleAuth(_googleAuth);
+          renderSigninButton(_gapi);
         })();
       });
     };
 
-    // Ensure everything is set before loading the script
-    loadGoogleScript(); // (Ref. 9)
+    loadGoogleScript();
+    console.log(name + " : " + email);
+
+    const readMemoUrl =
+      "https://script.googleapis.com/v1/scripts/AKfycbwCXY7YKm7S1VJv5xGItcsOJuT0JLuvM7wyrm8rrY6H9lcPZ99hGiU3MrRlwV6CKqXV7Q:run";
+
+    axios({
+      method: "post",
+      url: readMemoUrl,
+      data: {
+        function: "readMemo",
+        parameters: [],
+      },
+    })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   }, []);
 
   const saveMemo = (m) => {
@@ -91,19 +84,6 @@ const Home = () => {
   return (
     <div className="homeItems">
       {!isLoggedIn && <div id="google-signin"></div>}
-
-      {isLoggedIn && (
-        <div>
-          <div>
-            <img src={imageUrl} />
-          </div>
-          <div>{name}</div>
-          <div>{email}</div>
-          <button className="btn-primary" onClick={logOut}>
-            Log Out
-          </button>
-        </div>
-      )}
       <div className="homeMemo">
         <div className="homeMemoBoxEdit">
           <SimpleMDE value={memo} onChange={(e) => saveMemo(e)} />
