65bd9bb0e07366c85fa20408bd2723049871a99d
さいかい <70578583+saikai-moviestudio@users.noreply.github.com>
2021-08-01 18:38:06 +0900

    ok

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,17 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Home from "./components/Home"
+import Home from './components/Home';
 
 const App = () => {
-
   return (
-
-      <BrowserRouter>
-        <Switch>
-        <Route path="/" exact children={<Home />} />
-        </Switch>
-      </BrowserRouter>
-
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
-
 
 export default App;
